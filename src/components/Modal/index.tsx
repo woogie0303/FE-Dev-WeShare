181501136e689d00acaeafe15b924b3db7d33af5
woogie0303 <rkdehddnr15123@gmail.com>
2024-02-19 18:07:50 +0900

    feat: 로그인 폼 모달창 기능 구현

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,42 +1,13 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React from 'react';
 import LoginForm from '../LoginForm';
 import ModalForm from './ModalForm';
-
-const preventScroll = () => {
-  const currentScrollY = window.scrollY;
-
-  document.body.style.position = 'fixed';
-  document.body.style.width = '100%';
-
-  document.body.style.overflowY = 'scroll';
-  document.documentElement.style.scrollBehavior = 'auto';
-  return currentScrollY;
-};
-
-const allowScroll = (prevScrollY: number) => {
-  const hi = 'st';
-  document.body.style.position = '';
-  document.body.style.width = '';
-  document.body.style.top = '';
-  document.body.style.overflowY = '';
-  window.scrollTo(0, prevScrollY);
-};
 
 export default function Modal() {
   const searchParam = useSearchParams();
   const modal = searchParam.get('modal');
-
-  useEffect(() => {
-    if (modal) {
-      const currentScrollY = window.scrollY;
-      document.body.style.position = 'fixed';
-
-      document.body.style.overflowY = 'hidden';
-    }
-  }, [modal]);
 
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
 export default function Navbar() {
+  const searchParam = useSearchParams();
+  const modal = searchParam.get('modal');
   return (
-    <nav className="fixed w-full z-20  bg-white flex items-center justify-between px-6 py-4 text-blue-600 shadow-nav  ">
+    <nav
+      className={`${modal ? 'top-0' : ''} fixed w-full bg-white flex items-center justify-between px-6 py-4 text-blue-600 shadow-nav z-30`}
+    >
       <Link href="/" className="text-3xl   font-extrabold">
         weShare
       </Link>
-      <div className="text font-bold [&>*:first-child]:mr-8 ">
+      <div className="flex items-center text font-bold [&>*:first-child]:mr-8 ">
         <Link href="/" className="">
           여행 쓰기
         </Link>
@@ -17,7 +22,7 @@ export default function Navbar() {
         <Link href="?modal=true">
           <button
             type="button"
-            className="bg-blue-100 px-4 pt-[0.6rem] py-2  rounded align-bottom"
+            className="bg-blue-100 px-4  py-2  rounded align-bottom"
           >
             로그인
           </button>
