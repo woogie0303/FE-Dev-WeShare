4cc9e1bfd582f68f18b8ae168a0ae1c1cfb4960c
woogie0303 <rkdehddnr15123@gmail.com>
2024-03-11 17:14:21 +0900

    feat: TravelEditListItem 삭제 기능 추가

---
+++ src/components/TravelEdit/TravelEditForm/TravelEditListItem.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { isEqual } from 'lodash';
+import Image from 'next/image';
+import React from 'react';
+
+type Props = {
+  activeVisitDate: string;
+  activeVisitPlace: EditListItem;
+  setVisitDatesArr: React.Dispatch<
+    React.SetStateAction<VisitDatesType<EditListItem>[]>
+  >;
+};
+
+export default function TravelEditListItem({
+  activeVisitDate,
+  activeVisitPlace,
+  setVisitDatesArr,
+}: Props) {
+  return (
+    <div className=" flex mx-8 p-4 gap-4 mb-6  shadow-travelEditItem items-center rounded-lg">
+      {/* image */}
+      <div className="relative w-[6rem] h-[5rem] ">
+        <Image
+          src="https://media.istockphoto.com/id/1137568153/photo/cherry-blossoms-in-spring-seoul-in-korea.jpg?s=612x612&w=0&k=20&c=SP8py_0WQrn4mMFdrd0bvk7-MfRWxJl6C3AsHxJq8ck="
+          alt="hi"
+          fill
+          className=" object-cover rounded-lg"
+        />
+      </div>
+      {/* Detail */}
+      <div className="w-full font-bold">
+        <div className="flex justify-between mb-4">
+          <p>{activeVisitPlace.title}</p>
+          <p>{activeVisitPlace.time}</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p>{activeVisitPlace.expense}원</p>
+          <div className="">
+            <button
+              type="button"
+              className="px-2 py-1 bg-gray-300 rounded-md mr-2"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              className="px-2 py-1 bg-gray-300 rounded-md"
+              onClick={() => {
+                setVisitDatesArr((pre) => {
+                  const copyVisitDatesArr = [...pre];
+                  const activePlacesIndex = copyVisitDatesArr.findIndex(
+                    (visitArr) => visitArr.travelDate === activeVisitDate,
+                  );
+                  copyVisitDatesArr[activePlacesIndex].visitPlaces =
+                    copyVisitDatesArr[activePlacesIndex].visitPlaces.filter(
+                      (visitPlace) => !isEqual(visitPlace, activeVisitPlace),
+                    );
+
+                  return copyVisitDatesArr;
+                });
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
