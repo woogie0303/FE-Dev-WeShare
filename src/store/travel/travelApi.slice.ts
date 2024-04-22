f3eb3fe9ba6cdf2556d6c429b2d1262737f24d7e
woogie0303 <rkdehddnr15123@gmail.com>
2024-04-22 13:19:19 +0900

    fix: 수정

---
+++ src/store/travel/travelApi.slice.ts
@@ -0,0 +1,73 @@
+import {
+  DayDetailType,
+  PageableResponseType,
+  PlaceItemType,
+  SchedulesType,
+  TravelPostType,
+} from '@/types/TravelType';
+import { apiSlice } from '../api/apiSlice';
+
+type TravelPostParams = {
+  destination?: string;
+  priceRange?: { startPrice: string; endPrice: string };
+  currentPageNum: number;
+};
+
+const travelApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getTravelPost: builder.query<
+      Pick<
+        PageableResponseType<TravelPostType>,
+        'content' | 'last' | 'pageable' | 'totalPages'
+      >,
+      TravelPostParams
+    >({
+      query: (travelPost) => ({
+        url: `/api/v1/trip/schedules?page=${travelPost.currentPageNum}&size=12${travelPost.destination?.length ? `&destination=${travelPost.destination}` : ''} ${travelPost.priceRange?.startPrice.length || travelPost.priceRange?.endPrice?.length ? `&expense=${travelPost.priceRange.startPrice}~${travelPost.priceRange.endPrice}` : ''}`,
+      }),
+      transformResponse: (data: PageableResponseType<TravelPostType>) => {
+        const { content, last, pageable, totalPages } = data;
+
+        return { content, last, pageable, totalPages };
+      },
+    }),
+    editTravelPost: builder.mutation<
+      void,
+      SchedulesType<DayDetailType<PlaceItemType>>
+    >({
+      query: (editTravelPost) => ({
+        url: '/api/v1/trip/schedules',
+        method: 'POST',
+        body: { ...editTravelPost },
+      }),
+    }),
+    getMyTravelPost: builder.query<
+      Pick<TravelPostType, 'title' | 'scheduleId' | 'createdAt'>[],
+      string
+    >({
+      query: (myScheduleArg) => ({ url: `/api/v1/me/${myScheduleArg}` }),
+      transformResponse: (res: any) => {
+        const { content } = res.data;
+
+        return content;
+      },
+    }),
+    upDateTravelPost: builder.mutation<
+      void,
+      SchedulesType<DayDetailType<PlaceItemType>>
+    >({
+      query: (editTravelPost) => ({
+        url: '/api/v1/trip/schedules',
+        method: 'PATCH',
+        body: { ...editTravelPost },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetTravelPostQuery,
+  useEditTravelPostMutation,
+  useGetMyTravelPostQuery,
+  useUpDateTravelPostMutation,
+} = travelApiSlice;
